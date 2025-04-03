aca4059db0901db4668f55258e6ae21bbcb698fb
Khánh Huy Phạm <khanhhuypham@Khanhs-Mac-mini.local>
2025-04-03 13:25:00 +0700

    add check win state

--- scripts/board.gd
+++ scripts/board.gd
@@ -4,8 +4,10 @@ class_name Board
 @export var cell: PackedScene
 @onready var grid_container: GridContainer = $GridContainer
 
+signal select_cell(col: int, row: int)
+
 enum Difficulty {
-	EASY = 40,
+	EASY = 1,
 	MEDIUM = 45,
 	HARD = 50,
 	EXPERT = 55,
@@ -18,19 +20,40 @@ var CELL_SIZE_WITH_SPACING: int = 35
 
 var valid_numbers: Array[int] = []
 
-var rows: Array[Array] = []
-var cols: Array[Array] = []
 var grid: Array = []
-
 var res_grid: Array = []
+var current_col: int = -1
+var current_row: int = -1
 
 var window_size = DisplayServer.window_get_size()
+
 
 func _ready() -> void:
 	init_board()
 	generate_grid()
 	apply_puzzle(Difficulty.EASY)
 	render()
+	cell.connect("change_value", check_winner)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var key_event: InputEventKey = event
+		if key_event.pressed && (key_event.keycode in [KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT]):
+			if current_col == -1 || current_row == -1:
+				current_col = 0
+				current_row = 0
+			else:
+				if key_event.keycode == KEY_UP:
+					current_row -= 1
+				if key_event.keycode == KEY_DOWN:
+					current_row += 1
+				if key_event.keycode == KEY_RIGHT:
+					current_col += 1
+				if key_event.keycode == KEY_LEFT:
+					current_col -= 1
+			emit_signal("select_cell", current_col, current_row)
+
+
 
 func render() -> void:
 	var init_pos: Vector2
@@ -41,9 +64,11 @@ func render() -> void:
 		for col in range(GRID_SIZE):
 			var new_button: Cell = cell.instantiate()
 			new_button.position = init_pos + Vector2(col * CELL_SIZE_WITH_SPACING, row * CELL_SIZE_WITH_SPACING)
+			new_button.correct_value = res_grid[row][col]
+			new_button.col = col
+			new_button.row = row
 			if grid[row][col] != 0:
 				new_button.text = str(grid[row][col])
-			new_button.correct_value = res_grid[row][col]
 			add_child(new_button)
 
 func apply_puzzle(difficulty: Difficulty):
@@ -123,3 +148,12 @@ func is_valid_row(row: int, num: int) -> bool:
 
 func is_valid_box(col: int, row: int, num: int) -> bool:
 	return num not in get_box(col, row)
+
+func check_winner() -> bool:
+	for row in range(GRID_SIZE):
+		for col in range(GRID_SIZE):
+			if grid[row][col] != res_grid[row][col]:
+				print("ALMOST")
+				return false
+	print("VICTORY")
+	return true
--- scripts/cell_button.gd
+++ scripts/cell_button.gd
@@ -1,12 +1,20 @@
 extends Button
 class_name Cell
 
+signal same_attribute(same_col: int, same_row: int, same_value_text: String)
+signal deactivate_same_attribute(same_col: int, same_row: int, same_value_text: String)
+signal change_value
+
 var focus: bool = false
 var correct_value: int = 0
+var col: int = -1
+var row: int = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#connect("select_cell", _on_focus_entered)
+	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,9 +33,24 @@ func _input(event: InputEvent) -> void:
 		for i in range(9):
 			if Input.is_key_pressed(KEY_1 + i):
 				text = str(i+1)
+				emit_signal("change_value")
+
+func hover_same_attributes() -> void:
+	pass
 
 func _on_focus_entered() -> void:
 	focus = true
+	emit_signal("same_attribute", col, row, text)
 
 func _on_focus_exited() -> void:
 	focus = false
+	emit_signal("deactivate_same_attribute", col, row, text)
+
+func _on_same_attribute(same_col: int, same_row: int, same_value_text: String) -> void:
+	if col == same_col || row == same_row || text == same_value_text:
+		focus = true
+
+
+func _on_deactivate_same_attribute(same_col: int, same_row: int, same_value_text: String) -> void:
+	if col == same_col || row == same_row || text == same_value_text:
+		focus = false
